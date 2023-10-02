aa0d22fcc76e8c1c412acbf4b5012fa2018a43d7
Ondřej Műller <devcoft@gmail.com>
2023-10-02 15:25:26 +0200

    prepare test package

--- packages/sample-project/vite.config.ts
+++ packages/sample-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { shadowDom } from '../../src/shadowDom'
+// import { rootRedirect } from '../../src/rootRedirect'
 import path from 'path'
 import Inspect from 'vite-plugin-inspect'
 
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    shadowDom({ logging: true }),
+    // rootRedirect(''),
     Inspect(),
   ],
 })
