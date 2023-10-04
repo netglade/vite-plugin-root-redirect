90725226a9927a1f11f1e5d5f594e3e03a42b20e
Ondřej Műller <devcoft@gmail.com>
2023-10-04 11:23:50 +0200

    add ESM and CJS projects, test their builds

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -3,23 +3,36 @@ import {execSync} from "child_process";
 import fs from "fs";
 import packageJson from "../package.json";
 
-const cwdAndInstallTestProject = (name: string) => {
-  const rootDir = resolve(__dirname, '..')
-  const testProjectsDir = resolve(rootDir, 'test-projects')
-  process.chdir(resolve(testProjectsDir, name))
-  execSync('npm install', { stdio: 'inherit' })
-
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nTesting setup end.  Now running test.')
-}
-
 beforeEach(() => {
   const rootDir = resolve(__dirname, '..')
   process.chdir(rootDir)
   execSync('npm run build', { stdio: 'inherit' })
   execSync('npm pack', { stdio: 'inherit' })
-  fs.renameSync(`netglade-vite-plugin-root-redirect-${packageJson.version}.tgz`, 'vite-plugin-root-redirect.tgz')
+  const packName = 'vite-plugin-root-redirect.tgz'
+  if (fs.existsSync(packName)) {
+    fs.unlinkSync(packName)
+  }
+  fs.renameSync(`netglade-vite-plugin-root-redirect-${packageJson.version}.tgz`, packName)
 })
 
-test("ES Install", () => {
-  cwdAndInstallTestProject('es-install')
+const cwdAndInstallTestProject = (name: string) => {
+  const rootDir = resolve(__dirname, '..')
+  const testProjectsDir = resolve(rootDir, 'test-projects')
+  process.chdir(resolve(testProjectsDir, name))
+  if (fs.existsSync('node_modules')) {
+    fs.rmSync('node_modules', { recursive: true, force: true })
+  }
+  execSync('npm install', { stdio: 'inherit' })
+
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nTesting setup end.  Now running test.')
+}
+
+test("ESM Install", () => {
+  cwdAndInstallTestProject('esm-install')
+  execSync('npm run build', { stdio: 'inherit' })
+});
+
+test("CJS Install", () => {
+  cwdAndInstallTestProject('cjs-install')
+  execSync('npm run test', { stdio: 'inherit' })
 });
